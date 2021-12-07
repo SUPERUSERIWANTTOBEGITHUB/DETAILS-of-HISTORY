541da1420c04678b8af044f79967659402f1ad9b
AS <yvg68@yandex.ru>
2021-12-07 17:51:15 +0300

    13

--- app/src/main/java/yvg68/measurer/crc9_20211201/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/crc9_20211201/MainActivity.kt
@@ -2,10 +2,20 @@ package yvg68.measurer.crc9_20211201
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var carRecyclerView: RecyclerView
+    //val carList: List<Car>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        carRecyclerView = findViewById(R.id.car_view)
+        carRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        carRecyclerView.adapter = CarAdapter(carList)
     }
 }
